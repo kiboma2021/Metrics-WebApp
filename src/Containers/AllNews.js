4d8edb12b0359d3c9d7bcf46fdd392aad231b822
Ben Kiboma <omayioben@gmail.com>
2022-07-25 00:25:27 +0300

    Dispatch an action AllNews

--- src/Containers/AllNews.js
+++ src/Containers/AllNews.js
@@ -1,16 +1,17 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import NewsComponent from './NewsComponents';
 
 const AllNews = () => {
   const allnews = useSelector((state) => state);
+  const dispatch = useDispatch();
 
   const fetchNews = async () => {
     const response = await axios.get('https://newsdata.io/api/1/news?apikey=pub_95321f99baef7d04ae3a33bc6518f0ac6fcc&country=us&language=en').catch((err) => {
       console.log('Err', err);
     });
-    console.log(response);
+    dispatch(response.data);
   };
 
   useEffect(() => {
